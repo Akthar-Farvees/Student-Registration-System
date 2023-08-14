aa54527b082cc9f2b39658deb10e9659ba021f04
Akthar farvees <junaideenakthar123@gmail.com>
2023-08-14 11:11:49 +0530

    Import files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { StudentRegistration } from './models/student-resitration';
+import { StudentResitrationService } from './service/student-resitration.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StuRegisSystem';
+  stuRegister: StudentRegistration [] = [];
+
+  constructor(private StudentResitrationService : StudentResitrationService) {}
+
+  ngOnInit() : void {
+    this.stuRegister = this.StudentResitrationService.getStudentRegistration();
+    console.log(this.stuRegister);
+  }
+
+
+
+  
 }
